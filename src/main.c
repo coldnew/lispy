4315ab20232a4fa00899f1986de5d973ed503615
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-07-08 15:57:29 +0800

    minimal prototype
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+const char *version = "0.0.1";
+
+int main(int argc, char *argv[])
+{
+        printf("MiniLisp - v%s", version);
+        printf("Press Ctrl-c to exit\n");
+
+        while (1) {
+
+        }
+
+        return 0;
+}
