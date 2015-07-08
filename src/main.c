c7d62985e0fa898c0fdc6d3c648ec6aa1f8e0c85
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-07-08 16:42:18 +0800

    Implement basic prompt by readline
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- src/main.c
+++ src/main.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
 const char *version = "0.0.1";
 
 int main(int argc, char *argv[])
 {
-        printf("MiniLisp - v%s", version);
+        printf("MiniLisp - v%s ", version);
         printf("Press Ctrl-c to exit\n");
 
         while (1) {
+                char *input = readline("lisp> ");
+                add_history(input);
 
+                printf("you type: %s\n", input);
+                free(input);
         }
 
         return 0;
