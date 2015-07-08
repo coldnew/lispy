02efe85162d5a1cf37dbea52b4ebeed3e8587d0f
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-07-08 22:10:57 +0800

    Use mpc as parser
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- src/main.c
+++ src/main.c
@@ -3,6 +3,9 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
+// 3rdparty/mpc
+#include "mpc.h"
+
 const char *version = "0.0.1";
 
 int main(int argc, char *argv[])
@@ -10,13 +13,42 @@ int main(int argc, char *argv[])
         printf("MiniLisp - v%s ", version);
         printf("Press Ctrl-c to exit\n");
 
+        /* Create Some Parsers */
+        mpc_parser_t* Number   = mpc_new("number");
+        mpc_parser_t* Operator = mpc_new("operator");
+        mpc_parser_t* Expr     = mpc_new("expr");
+        mpc_parser_t* Lispy    = mpc_new("lispy");
+
+        /* Define them with the following Language */
+        mpca_lang(MPCA_LANG_DEFAULT,
+                  "                                                     \
+                   number   : /-?[0-9]+/ ;                              \
+                   operator : '+' | '-' | '*' | '/' ;                   \
+                   expr     : <number> | '(' <operator> <expr>+ ')' ;   \
+                   lispy    : /^/ <operator> <expr>+ /$/ ;              \
+                  ", Number, Operator, Expr, Lispy);
+
         while (1) {
                 char *input = readline("lisp> ");
                 add_history(input);
 
-                printf("you type: %s\n", input);
+                /* Attempt to parse the user input */
+                mpc_result_t r;
+                if (mpc_parse("<stdin>", input, Lispy, &r)) {
+                        /* On success print and delete the AST */
+                        mpc_ast_print(r.output);
+                        mpc_ast_delete(r.output);
+                } else {
+                        /* Otherwise print and delete the Error */
+                        mpc_err_print(r.error);
+                        mpc_err_delete(r.error);
+                }
+
                 free(input);
         }
+
+        /* Undefine and delete our parsers */
+        mpc_cleanup(4, Number, Operator, Expr, Lispy);
 
         return 0;
 }
